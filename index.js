c26f32b723c633a0df27156fab36a3378577089d
Антон Киршанов <maetchkin@gmail.com>
2020-05-22 17:25:02 +0300

    up, clean

--- index.js
+++ index.js
@@ -3,43 +3,22 @@ const YAML        = require('yaml');
 const YAMLCST     = require('yaml/dist/constants.js').Type;
 const path        = require('path');
 
-
 const getNStr = (node, next) => {
     const {type, contents, items} = node;
-    let res = node;
-/*
-ALIAS
-BLANK_LINE
-BLOCK_FOLDED
-BLOCK_LITERAL
-COMMENT
-DIRECTIVE
-DOCUMENT
-FLOW_MAP
-FLOW_SEQ
-
-    MAP
-
-MAP_KEY
-MAP_VALUE
-PLAIN
-QUOTE_DOUBLE
-QUOTE_SINGLE
-SEQ
-SEQ_ITEM
-*/
-
+    let res = null;
     switch (type) {
+        case YAMLCST.SEQ:
+            if(items){
+                res = items.indexOf( next );
+            };
+        break;
         case YAMLCST.MAP:
             if(items){
                 let pos = items.indexOf( next );
                 res = items[pos-1].toString();
             };
         break;
-
-        default: res = null
     }
-
     return {type, res};
 }
 
@@ -51,70 +30,49 @@ const tagInclude = ctx => ({
         const {strValue} = cst;
         const file = path.join(context, strValue);
 
-        let ypath = [cst];
-        let p;
+        let cstPath = [cst];
+        let parent;
+        do {
+            parent = cstPath[0]?.context?.parent;
+            parent && (cstPath = [parent, ...cstPath]);
+        } while (parent);
 
-        while (p = ypath[0]?.context?.parent){
-            ypath = [p, ...ypath];
-        }
+        let ypath = cstPath
+                        .reduce(
+                            (acc, n, ni, arr) => {
+                                n.type && acc.push( getNStr( n, arr[ni+1] ) );
+                                return acc;
+                            },
+                            []
+                        )
+                        .map( ({res}) => res )
+                        .filter( n => !!n )
+        ;
 
-        let ypath2 = ypath.reduce(
-                        (acc, n, ni, arr) => {
-                            n.type && acc.push( getNStr( n, arr[ni+1] ) );
-                            return acc;
-                        },
-                        []
-                    ).map( ({res}) => res ).filter( n => !!n ).join('.');
-        //console.log('YAMLCST', YAMLCST );
-        //console.log('ypath2', ypath2 );
-        //console.log('cst', {/*doc,*/ /*ypath, */ypath2, strValue} );
-
-
-        console.log('!INCLUDE', { /*context, value, */file, ypath2 } );
-        return {
-            include:file,
-            ypath2
-            //cst
-        };
+        console.log('!INCLUDE', {file, ypath});
+        return { file, ypath };
     }
 });
-
-//YAML.defaultOptions.customTags = [ include ];
-
-
-
 
 function getModule (content) {
     const ctx = this;
     const { resourcePath, resource, request, rootContext, context, query } = this;
     const options = loaderUtils.getOptions(this);
 
-    //console.log("\n\n\n", resourcePath, "\n=================\n", { options, resource, request, rootContext, context, query });
-
-    /*console.log({ resourcePath, resource, request, rootContext, context });*/
-
-
-
 
     return new Promise(
         (resolve, reject) => {
             try {
                 if (content) {
-                    const stream = YAML.parseAllDocuments(
-                        content, {
-                            prettyErrors: true,
-                            keepCstNodes: true,
-                            customTags: [ tagInclude(ctx) ]
-                        }
-                    );
-                    const res = [];
-                    for (const doc of stream) {
-                      for (const warn of doc.warnings) { ctx.emitWarning(warn); }
-                      for (const err of doc.errors)    { ctx.emitError(err)};
-                      res.push(doc.toJSON())
-                    }
-                    const m = `module.exports = ${ JSON.stringify(res) }`;
-                    resolve( m );
+                    const doc = YAML.parse(
+                                    content,
+                                    {
+                                        prettyErrors: true,
+                                        keepCstNodes: true,
+                                        customTags: [ tagInclude(ctx) ]
+                                    }
+                                );
+                    resolve( `module.exports = ${ JSON.stringify(doc) }` );
                 } else {
                     throw new Error('no content in ' + resourcePath);
                 }
@@ -124,7 +82,6 @@ function getModule (content) {
         }
     );
 };
-
 
 function multiYamlLoader (content) {
     const callback = this.async();
@@ -139,12 +96,3 @@ function multiYamlLoader (content) {
 }
 
 module.exports = multiYamlLoader;
-
-
-/*
-if (loaderUtils.isUrlRequest(url)) {
-  // Logic for requestable url
-  const request = loaderUtils.urlToRequest(url);
-}
-
-*/
